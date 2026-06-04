0c990df3ed5ee953e1e6fe95a4664ada970adeee
nsfida <nadeemshahzadfida@outlook.com>
2026-06-04 19:15:18 +0400

    Delete NCoin directory

--- NCoin/ncoin/__init__.py
+++
@@ -1,6 +0,0 @@
-"""NCoin core package."""
-
-from .constants import COIN, MAX_MONEY
-from .wallet import Wallet
-
-__all__ = ["COIN", "MAX_MONEY", "Wallet"]
--- NCoin/ncoin_core.py
+++
@@ -1,7 +0,0 @@
-"""NCoin Core launcher."""
-
-from ncoin.cli import main
-
-
-if __name__ == "__main__":
-    main()
